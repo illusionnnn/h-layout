600c38b28ff06eaaceac886dbef6af343093ff0b
Hedgehog96 <1198659788@qq.com>
2022-08-31 14:19:27 +0800

    [fix]: disable the label editor

--- src/views/r-aside/PropertyEditor/LabelEditor.vue
+++ src/views/r-aside/PropertyEditor/LabelEditor.vue
@@ -4,7 +4,7 @@
  * @Author: Hedgehog96
  * @Date: 2022-07-21 16:51:59
  * @LastEditors: Hedgehog96
- * @LastEditTime: 2022-07-28 01:02:14
+ * @LastEditTime: 2022-08-31 14:17:45
 -->
 <template>
     <el-form-item class="h-editor">
@@ -18,6 +18,7 @@
             <el-input
                 v-model="props.elem.label"
                 type="text"
+                disabled
             />
         </div>
     </el-form-item>
